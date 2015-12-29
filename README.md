c00d9d2c3ca42d47d215e1ca0bc01b2ede9fd23e
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-12-29 18:16:07 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,35 @@
+# ShowTopsAndBottoms.glyphsReporter
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert. It displays the bounding box tops and bottoms for each glyph in the Edit view, and marks them red if they are not inside an alignment zone.
+
+![Tops and Bottoms.](ShowTopsAndBottoms.png "Show Tops and Bottoms")
+
+After installation, it will add the menu item *View > Show Tops and Bottoms*. You can set a keyboard shortcut in System Preferences.
+
+### Installation
+
+1. Download the complete ZIP file and unpack it, or clone the repository.
+2. Double click the .glyphsReporter file. Confirm the dialog that appears in Glyphs.
+3. Restart Glyphs
+
+### Usage Instructions
+
+1. Open a glyph in Edit View.
+2. Use *View > Show Tops and Bottoms* to toggle the display of bbox tops and bottoms.
+
+### Requirements
+
+The plugin works in Glyphs 2.2 or later, on OS X 10.11 or later. I can only test it in current app and OS versions, and perhaps it works on earlier versions too.
+
+### License
+
+Copyright 2015 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ ShowTopsAndBottoms.glyphsReporter/Contents/Resources/ShowTopsAndBottoms.py
@@ -0,0 +1,103 @@
+# encoding: utf-8
+
+
+from plugin import *
+from AppKit import *
+
+
+
+class ShowTopsAndBottoms(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = 'Tops and Bottoms'
+		
+	def drawForeground(self, layer):
+		pass
+
+	def drawTop( self, bbox, drawColor, zones ):
+		self.drawTopOrBottom( bbox, drawColor, zones, True )
+		
+	def drawBottom( self, bbox, drawColor, zones ):
+		self.drawTopOrBottom( bbox, drawColor, zones, False )
+		
+	def drawTopOrBottom( self, bbox, defaultColor, zones, top ):
+		try:
+			bboxOrigin = bbox.origin
+			bboxSize   = bbox.size
+			left = bboxOrigin.x
+			right = left + bboxSize.width
+			middle = left + bboxSize.width / 2.0
+			position = bboxOrigin.y
+			
+			offset = 20.0
+			surplus = 30.0
+			scale = self.getScale()
+			numberDistance = (offset+5.0) / scale
+			lineDistance = (offset-10.0) / scale
+
+			# adjust values for top/bottom:
+			if top:
+				position += bboxSize.height
+				numberDistance -= 10.0 / scale
+			else:
+				numberDistance *= -1
+				lineDistance *= -1
+			
+			# brings macro window to front and clears its log:
+			
+			
+			# draw it red if it is not inside a zone:
+			drawColor = NSColor.redColor()
+			for thisZone in zones:
+				zoneBegin = thisZone[0]
+				zoneEnd = zoneBegin + thisZone[1]
+				zoneBottom = min( ( zoneBegin, zoneEnd ) )
+				zoneTop = max( ( zoneBegin, zoneEnd ) )
+				if position <= zoneTop and position >= zoneBottom:
+					drawColor = defaultColor
+
+			# set line attributes:
+			drawColor.set()
+			NSBezierPath.setDefaultLineWidth_( 1.0/scale )
+			
+			# draw horizontal line on canvas:
+			leftPoint = NSPoint( left-surplus, position )
+			rightPoint = NSPoint( right+surplus, position )
+			NSBezierPath.strokeLineFromPoint_toPoint_( leftPoint, rightPoint )
+			
+			# draw vertical line on canvas:
+			startPoint = NSPoint( middle, position )
+			endPoint = NSPoint( middle, position+lineDistance )
+			NSBezierPath.strokeLineFromPoint_toPoint_( startPoint, endPoint )
+			
+			# draw number on canvas:
+			self.drawTextAtPoint( "%.1f" % position, NSPoint(middle,position+numberDistance), fontColor=drawColor )
+		except Exception as e:
+			self.logToConsole( "drawBottom: %s" % str(e) )
+	
+	def drawTopsAndBottoms( self, layer, defaultColor ):
+		try:
+			bbox = layer.bounds
+			if bbox.size.height > 0.0:
+				zones = [(int(z.position), int(z.size)) for z in layer.associatedFontMaster().alignmentZones]
+				topZones = [z for z in zones if z[1] > 0]
+				bottomZones = [z for z in zones if z[1] < 0]
+				self.drawTop( bbox, defaultColor, topZones )
+				self.drawBottom( bbox, defaultColor, bottomZones )
+		except Exception as e:
+			self.logToConsole( "drawTopsAndBottoms: %s" % str(e) )
+	
+	def drawBackground( self, layer ):
+		try:
+			self.drawTopsAndBottoms( layer, NSColor.darkGrayColor() )
+		except Exception as e:
+			self.logToConsole( "drawBackground: %s" % str(e) )
+
+	def drawBackgroundForInactiveLayers(self, layer):
+		try:
+			self.drawTopsAndBottoms( layer, NSColor.lightGrayColor() )
+		except Exception as e:
+			self.logToConsole( "drawBackgroundForInactiveLayers: %s" % str(e) )
+
+	def drawPreview(self, layer):
+		pass
